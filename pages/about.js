30968adb426962b9d145f868ffa98415c1b787cb
kokeshi <45554960+kokeshes@users.noreply.github.com>
2026-01-30 13:16:00 +0900

    Delete pages directory

--- pages/about.js
+++
@@ -1,4 +0,0 @@
-import { startNoise, enableGlitchPulse, registerSW } from "../assets/js/common.js";
-registerSW();
-const noise=document.getElementById("noise"); if(noise) startNoise(noise);
-enableGlitchPulse(document);
